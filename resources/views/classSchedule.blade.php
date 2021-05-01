79336dba283c5782cd619663d7b01c5cc3ad74de
stetlar <iatmanegara.22@gmail.com>
2021-05-01 22:48:23 +0700

    replace asset and link fix

--- resources/views/classSchedule.blade.php
+++ resources/views/classSchedule.blade.php
@@ -5,13 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Class schedule</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="C:/Users/user/Desktop/Class Schedul Project/Class-Schedule-Board/public/Assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="C:/Users/user/Desktop/Class Schedul Project/Class-Schedule-Board/public/Assets/css/untitled.css">
 </head>
 
-<body style="background-color: rgb(255,255,255);">
+<body>
     <div class="row" style="margin-top: 19px;">
-        <div class="col offset-xl-0 text-center"><img src="assets/img/ESQ-Business-School-logo.png" style="width: 130px;"></div>
+        <div class="col offset-xl-0 text-center"><img src="C:/Users/user/Desktop/Class Schedul Project/Class-Schedule-Board/public/Assets/img/ESQ-Business-School-logo.png" style="width: 130px;"></div>
         <div class="col">
             <h2 class="text-center">SELAMAT DATANG DI ESQ BUSINESS SCHOOL</h2>
         </div>
@@ -19,100 +19,45 @@
             <h2>RABU 28-04-2021</h2>
         </div>
     </div>
-    <div class="row" style="margin-top: 33px;">
-        <div class="col text-center" style="background-color: #be7575;">
-            <h2 class="text-center" style="margin-top: 10px;">Waktu</h2>
-        </div>
-        <div class="col" style="background-color: #be7575;">
-            <h2 class="text-center" style="margin-top: 10px;">Kode</h2>
-        </div>
-        <div class="col" style="background-color: #be7575;">
-            <h2 class="text-center" style="margin-top: 10px;">Matkul</h2>
-        </div>
-        <div class="col" style="background-color: #be7575;">
-            <h2 class="text-center" style="margin-top: 10px;">Dosen</h2>
-        </div>
-        <div class="col" style="background-color: #be7575;">
-            <h2 class="text-center" style="margin-top: 10px;">Semester</h2>
-        </div>
-        <div class="col" style="background-color: #be7575;">
-            <h2 class="text-center" style="margin-top: 10px;">Ruang</h2>
-        </div>
-        <div class="col" style="background-color: #be7575;">
-            <h2 class="text-center" style="margin-top: 10px;">Status</h2>
-        </div>
-    </div>
-    <div class="row" style="margin-top: -1px;">
-        <div class="col text-center" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">07:30 - 10:00</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">KBMT308</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">Statistika R</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">Danang Indrajaya / Abdul Barir Hakim</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">6, 8-BIS</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">R. 1901</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">On Schedule</h4>
+    <div class="table-bordered">
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Column 1</th>
+                        <th>Column 2</th>
+                        <th>Column 3</th>
+                        <th>Column 3</th>
+                        <th>Column 3</th>
+                        <th>Column 3</th>
+                        <th>Column 3</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Cell 1</td>
+                        <td>Cell 2</td>
+                        <td>Cell 3</td>
+                        <td>Cell 3</td>
+                        <td>Cell 3</td>
+                        <td>Cell 3</td>
+                        <td>Cell 3</td>
+                    </tr>
+                    <tr>
+                        <td>Cell 3</td>
+                        <td>Cell 4</td>
+                        <td>Cell 3</td>
+                        <td>Cell 3</td>
+                        <td>Cell 3</td>
+                        <td>Cell 3</td>
+                        <td>Cell 3</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
-    <div class="row" style="margin-top: -1px;">
-        <div class="col text-center" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">07:30 - 10:00<br></h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">KKAI300</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">Kecerdasan Buatan</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">Edo Surya Utama</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">4-CS</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">L. 1801</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">Batal Kelas</h4>
-        </div>
-    </div>
-    <div class="row" style="margin-top: -1px;">
-        <div class="col text-center" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">07:30 - 10:00<br></h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">KBSE402</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">Manajemen Proyek</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">Ahlijati Nuraminah</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">6-CS</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">R.1804</h4>
-        </div>
-        <div class="col" style="background-color: #f1b9b9;">
-            <h4 class="text-center" style="margin-top: 10px;">Sedang Berlangsung</h4>
-        </div>
-    </div>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="C:/Users/user/Desktop/Class Schedul Project/Class-Schedule-Board/public/Assets/js/jquery.min.js"></script>
+    <script src="C:/Users/user/Desktop/Class Schedul Project/Class-Schedule-Board/public/Assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
