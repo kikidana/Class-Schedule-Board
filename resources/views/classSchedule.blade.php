fb8cedfbf0dfc51216db9fdecdb92a8ff463a4a5
stetlar <iatmanegara.22@gmail.com>
2021-05-13 22:16:47 +0700

    fix script for datatable

--- resources/views/classSchedule.blade.php
+++ resources/views/classSchedule.blade.php
@@ -56,7 +56,7 @@
     <script>src="https://code.jquery.com/jquery-3.5.1.js"</script>
     <script>
     $(document).ready(function() {
-        $('tabelsesi').DataTable();
+        $('#tabelsesi').DataTable();
     } );
     </script>
 </body>
